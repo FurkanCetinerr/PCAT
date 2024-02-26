d7b27a458ecef7739fdba49069b4891aa7d15fe8
Furkan Çetiner <furkanctiner@gmail.com>
2024-02-26 19:46:57 +0300

    Pagination

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -3,9 +3,19 @@ const fs= require('fs');
 const path = require("path");
 
 exports.getAllPhotos= async (req, res) => {
-    const photos = await Photo.find({}).sort({ dateCreated: -1 });
+  const page= req.query.page || 1;
+  const photosPerPage= 3;
+
+  const totalPhotos= await Photo.find().countDocuments();
+  const photos = await Photo.find({})
+  .sort({ dateCreated: -1 })
+  .skip((page-1)*photosPerPage)
+  .limit(photosPerPage);
+
     res.render("index", {
-      photos,
+      photos:photos,
+      current:page,
+      pages:Math.ceil(totalPhotos/photosPerPage)
     });
   }
 exports.getPhoto= async (req, res) => {
